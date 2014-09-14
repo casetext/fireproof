40eb01ae4197bb34e3afdb7060f084ef66ec6ad3
Harry Schmidt <goldibex@casetext.com>
2014-09-13 19:11:19 -0700

    Snapshot tests now pass

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 'use strict';
 
+var Fireproof = require('./lib/fireproof');
+require('./lib/query')(Fireproof);
+require('./lib/read')(Fireproof);
+require('./lib/write')(Fireproof);
+require('./lib/onDisconnect')(Fireproof);
+
 module.exports = {
-  Fireproof: require('./lib/Fireproof'),
+  Fireproof: Fireproof,
   FireproofSimpleLogin: require('./lib/FireproofSimpleLogin')
 };
--- test/spec/lib/Snapshot.js
+++ test/spec/lib/Snapshot.js
@@ -1,12 +1,75 @@
 
 'use strict';
 
+var Fireproof = require('../../../index').Fireproof,
+  Q = require('kew');
+
+Fireproof.bless(Q);
+
 // on Snapshot, only forEach and ref need to be tested, and just to make sure
 // they hand back the correct kind of delegate object.
 
-describe('Fireproof', function() {
+describe('FireproofSnapshot', function() {
 
-  describe('data snapshots', function() {
+  describe('#forEach', function() {
+
+    var fireproof;
+
+    before(function() {
+
+      fireproof = new Fireproof(firebase);
+
+      return fireproof.child('snapTest')
+      .set({
+        foo: 'bar',
+        baz: 'quux',
+        bells: 'whistles'
+      });
+
+    });
+
+    it('iterates over the children of the snapshot', function() {
+
+      return fireproof.child('snapTest')
+      .then(function(snap) {
+
+        snap.forEach(function(child) {
+          expect(child.constructor.name).to.equal('FireproofSnapshot');
+          expect(child.name()).to.match(/^(foo|baz|bells)$/);
+          expect(child.val()).to.match(/^(bar|quux|whistles)$/);
+        });
+
+      });
+
+    });
+
+  });
+
+  describe('#ref', function() {
+
+    var fireproof;
+
+    before(function() {
+
+      fireproof = new Fireproof(firebase);
+
+      return fireproof.child('snapTest')
+      .set({
+        foo: 'bar',
+        baz: 'quux',
+        bells: 'whistles'
+      });
+
+    });
+
+    it('returns the Fireproof reference associated with the snap', function() {
+
+      return fireproof.child('snapTest')
+      .then(function(snap) {
+        expect(snap.ref().constructor.name).to.equal('Fireproof');
+      });
+
+    });
 
   });
 
