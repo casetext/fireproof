4bdd3e4a538c08ccb3ab7745d40d8fc121c7f1e1
Harry Schmidt <goldibex@casetext.com>
2014-10-18 14:45:20 -0700

    Remove FireproofSimpleLogin

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ require('./lib/query')(Fireproof);
 require('./lib/read')(Fireproof);
 require('./lib/write')(Fireproof);
 require('./lib/on-disconnect')(Fireproof);
-require('./lib/fireproof-simple-login')(Fireproof);
 
 module.exports = Fireproof;
