09e28c1a4f769f823782edc910c46c8e18a383be
Harry Schmidt <goldibex@casetext.com>
2014-10-18 14:46:15 -0700

    Add lib/auth and lib/user

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ require('./lib/query')(Fireproof);
 require('./lib/read')(Fireproof);
 require('./lib/write')(Fireproof);
 require('./lib/on-disconnect')(Fireproof);
+require('./lib/auth')(Fireproof);
+require('./lib/user')(Fireproof);
 
 module.exports = Fireproof;
