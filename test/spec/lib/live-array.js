9bd9fe4f6ab8c05e53835f43e76a4582b3896a1d
Harry Schmidt <goldibex@casetext.com>
2014-12-11 14:44:07 -0800

    Removed some _nextTicks that weren't strictly necessary

--- test/spec/lib/live-array.js
+++ test/spec/lib/live-array.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-describe.only('Fireproof.LiveArray', function() {
+describe('Fireproof.LiveArray', function() {
 
   function shuffle(o) {
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x) {}
